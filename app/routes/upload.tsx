9360530b0c574089c5aeecfb112e9c025cd3ad11
HarshLOK <harshlok05@gmail.com>
2025-09-14 02:57:36 +0530

    fix: load icon correctly in FileUploader (/images/info.svg) and correct alt/class typos

--- app/routes/upload.tsx
+++ app/routes/upload.tsx
@@ -1,14 +1,17 @@
-import React, {useState} from 'react';
+import React, {type FormEvent, useState} from 'react';
 import Navbar from "~/components/Navbar";
+import FileUploader from "~/components/FileUploader";
 
 const Upload = () => {
-    const [isProcessing, setIsProcessing] = useState(true);
+    const [isProcessing, setIsProcessing] = useState(false);
     const [statusText, setStatusText] = useState('');
+    const handleSubmit =  (e: FormEvent<HTMLFormElement>) => {
+    }
     return (
         <main className="bg-[url('/images/bg-main.svg')] bg-cover">
             <Navbar />
             <section className="main-section">
-            <div className="page-heading">
+            <div className="page-heading py-16">
             <h1>Smart feedback for your dream job</h1>
                 {isProcessing ? (
                     <>
@@ -18,6 +21,28 @@ const Upload = () => {
                 ):(
                     <h2>Drop your resume for an ATS score and improvement tips</h2>
                 )}
+                {!isProcessing && (
+                    <form id="upload-form" onSubmit={handleSubmit} className={"flex flex-col gap-4 mt-8"}>
+                        <div className="form-div">
+                            <label htmlFor="company-name"> Comapny Name </label>
+                            <input type="text" name="company-name" placeholder="Company Name" id="company-name" />
+                        </div>
+                        <div className="form-div">
+                            <label htmlFor="job-title"> Job Title </label>
+                            <input type="text" name="job-title" placeholder="Job Title" id="job-title" />
+                        </div>
+                        <div className="form-div">
+                            <label htmlFor="job-description"> Job Description </label>
+                            <textarea rows={5} name="job-description" placeholder="Job Description" id="job-description" />
+                        </div>
+                        <div className="form-div">
+                            <label htmlFor="uploader">Upload Resume</label>
+                            <FileUploader/>
+                        </div>
+                        <button className="primary-button" type="submit">Upload</button>
+                    </form>
+                )}
+
             </div>
         </section>
             </main>
